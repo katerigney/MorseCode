ed75023daf3a7fc7b8de0d953321805c70413744
Rigney <krigney@proforma.com>
2018-04-04 14:00:21 -0400

    I think I did it!

--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO; //ask Mark why this did not come in?
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,66 @@ namespace MorseCode
     {
         static void Main(string[] args)
         {
+
+            var morseLibrary = new Dictionary<char, string>();
+
+            const string libraryFilePath = "../../morse.csv";
+
+
+            //The program should read the conversion data from a file and build a Dictionary in memory to help in the conversion (StreamReader)
+            using (var readFile = new StreamReader(libraryFilePath))
+            {
+                while (readFile.Peek() > -1)
+                {
+                    var letterInfo = readFile.ReadLine().Split(',');
+                    morseLibrary.Add(Convert.ToChar(letterInfo[0]), letterInfo[1]);
+                    //  foreach (var letter in morseLibrary)
+                    //{
+                        //        Console.WriteLine($"{letter.Key}, {letter.Value}");
+
+                        //  }
+                    }
+                }
+
+                //The Program should ask user if they have another word convert, if yes, then repeat the process
+                var programIsRunning = true;
+
+                while (programIsRunning)
+                {
+                    //As a user, I should be able to type in a phrase to convert. This should include letters and numbers
+                    Console.WriteLine("Let's translate some Morse Code! What would you like to translate?");
+                    var response = Console.ReadLine();
+                    var letters = response.ToUpper().ToCharArray();
+                    var morseTranslation = "";
+
+
+                    //The Program should convert the text that the use typed in to Morse Code
+
+                    //break apart string into characters
+                    //for each character it should find and return that value (morse translation) should be stored and added to a new string
+                    foreach (var letter in letters)
+                    {
+                        if (morseLibrary.ContainsKey(letter))
+                        {
+                            morseTranslation = morseTranslation + morseLibrary[letter];
+                        }
+                    }
+
+
+
+
+                    //The Program should display the converted text to the user
+                    Console.WriteLine($"{morseTranslation}");
+
+
+                }
+
+
+                //Adventure
+                //Save the User's past results and display them if the user asks. This should persist across multiple runnings of the app (read/write to a file)
+                //Allow the user to type in Morse Code and convert to English (need spaces between characters)
+                Console.ReadLine();
+            }
         }
     }
-}
+
